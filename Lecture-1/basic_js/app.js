170d642052ce8feb80783ec47cc24a573b679a0e
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-09-27 10:00:14 +0530

    lecture 1 pushed

---
+++ Lecture-1/basic_js/app.js
@@ -0,0 +1,11 @@
+
+// let abc = 10;
+// let samVohra = true;
+
+// print 
+// console.log(abc);
+// console.log(samVohra);
+
+// --------------------------------
+
+
---
+++ Lecture-1/number/app.js
@@ -0,0 +1,65 @@
+
+// numbers
+
+// let ab = -10; //integer
+// let abc = 10; //integer
+// let abcd = 10.10; //float
+
+// console.log( typeof(ab) ); // number
+// console.log( typeof(abc) ); // number
+// console.log( typeof(abcd) ); // number
+
+// ----------------------------------------------------
+
+// string () => 4 => 3 => "" , '' , `` (backticks)
+// let ab = "sam";
+// let abc = 'samarth';
+// let abcd = `samarth vohra`;
+
+// proof -> typeof
+
+// console.log( typeof(ab) ); // string
+// console.log( typeof(abc) ); // string
+// console.log( typeof(abcd) ); // string
+
+let namee = "samarth vohra";
+let city = "delhi";
+let role = "developer";
+// hi, i am (name) i am from (city) and i am a (role);
+
+//way-1
+// concatination (+) -> join
+let output = "hi i am " + namee + " i am from " + city + " and i am a  " + role;
+console.log(output);
+// way-2 (string template literals)
+
+// {} => swarg ka darwaza , baba concepts
+let output2 = `hi i am ${namee} i am from ${city} and i am a ${role}`;
+console.log(output2);
+
+
+
+// ----------------------------------------------------
+
+// Boolean (t/f)
+// let ab = true;
+// let abc = false;
+
+// console.log( typeof(ab) )
+// console.log( typeof(abc) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
